2a9b9afa7707e0a50d65a3ae0c86b9233babbf85
Saqlain Shah <saqlainshahbaltee@gmail.com>
2024-05-02 20:05:15 +0500

    added navabr

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -3,20 +3,23 @@ import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import LoginPage from "./components/Login";
 import RegisterPage from "./components/Register";
 import LandingPage from "./components/LanndingPage";
-import PersonalInfoForm from "./components/formikForm";
+import PersonalInfoForm from "./components/formikForm.jsx";
 // import { ThemeProvider } from "@emotion/react";
 // import ThemeConsumer from "./components/ThemeConsumer";
-import MultiStepForm from "./components/Index";
+import MultiStepForm from "./components/FormikForm/Index.jsx";
 import TableWithLocalizationProvider from "./components/Table.jsx";
-
+import ProductList from "./components/ProductList.jsx"
+import Navbar from "./Navbar.jsx"
 function App() {
   
   return (
     <React.Fragment>
       <Router>
+      <Navbar/>
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/products" element={<ProductList />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/formik" element={<PersonalInfoForm />} />
           <Route path="/multi-step" element={<MultiStepForm />} />
