2fca348b33f64d10296fb4c1fdf117fff918577f
AliGultary <muhammadalimhammadi@gmail.com>
2024-05-08 09:02:31 -0700

    added

--- admin-panel/src/App.jsx
+++ admin-panel/src/App.jsx
@@ -1,12 +1,14 @@
+// App.jsx
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Sidebar from './Sidebar';
 
-import Sidebar from "./Sidebar"
-function App() {
-
+export default function App() {
   return (
-    <>
-      <Sidebar/>
-    </>
-  )
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <Sidebar />
+    </Box>
+  );
 }
-
-export default App
