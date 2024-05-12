64e2c3c2f3d9ebd0bd3a1f0b3512e439e9354249
PervezHotovi <pervezabbas77@gmail.com>
2024-05-12 22:56:39 +0500

    ProductList and ProductDetails components added

--- admin-panel/src/App.jsx
+++ admin-panel/src/App.jsx
@@ -1,14 +1,18 @@
-// App.jsx
-import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Sidebar from './component/sidebar';
+// import MainContent from "./component/MainContent"
+// import Header from "./component/header"
+// import Sidebar from "./component/sidebar"
 
-export default function App() {
+import Sidebar from "./Sidebar"
+function App() {
+
   return (
-    <Box sx={{ display: 'flex' }}>
-      
-      <Sidebar />
-    </Box>
-  );
+    <>
+      <Sidebar/>
+      {/* <Header></Header>
+      <Sidebar/>
+      <MainContent/> */}
+    </>
+  )
 }
+
+export default App
