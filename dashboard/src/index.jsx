860f10cc9abf2d2afe6f558fa9b03bbd20a465b7
Kamran-Aqeel <kamu.jee.1234@gmail.com>
2024-09-11 20:38:17 +0500

    add this new file

--- dashboard/src/index.jsx
+++ dashboard/src/index.jsx
@@ -1,34 +1,36 @@
 import { createRoot } from 'react-dom/client';
 
-// fonts
+// ==============================|| FONTS ||============================== //
 import 'assets/fonts/inter/inter.css';
 
-// scroll bar
+// ==============================|| SCROLL BAR ||============================== //
 import 'simplebar/dist/simplebar.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-// third-party
+// ==============================|| THIRD-PARTY CSS ||============================== //
 import 'assets/third-party/apex-chart.css';
+
+// Redux provider for global state management
 import { Provider as ReduxProvider } from 'react-redux';
 
-// project-imports
+// ==============================|| PROJECT IMPORTS ||============================== //
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { store } from 'store';
+import { store } from './store'; // Make sure the path to the store is correct
 
+// Get the root element to attach React app
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-// ==============================|| MAIN - REACT DOM RENDER  ||============================== //
-
+// ==============================|| MAIN - REACT DOM RENDER ||============================== //
 root.render(
   <ReduxProvider store={store}>
     <App />
   </ReduxProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
+// ==============================|| REPORT WEB VITALS ||============================== //
+// If you want to start measuring performance in your app, pass a function to log results
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
